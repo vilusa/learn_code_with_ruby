77b200d6cbf74866b7b230610d30d8fc72d320cc
Kemal AKIN <kemal.akin@lab2023.com>
2019-04-27 17:26:03 +0300

    Repo initialized

---
+++ the_puts_methods.rb
@@ -0,0 +1,17 @@
+puts "Hello Ruby!"
+puts "I'm alive and well!"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%!"
+
+puts 15
+puts 3.1233123
+
+puts 6 + 5
+puts "6" + "5" # output is 65
+
+puts
+puts
+puts
+
+puts "See, there's a bunch of line break above!"
