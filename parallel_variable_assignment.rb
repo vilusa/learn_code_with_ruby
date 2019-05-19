e618247ab8e6d1cabfc20bc88837111021bb02a9
Kemal AKIN <kemal.akin@lab2023.com>
2019-05-19 16:37:56 +0300

    Parallel variable assigment operation and implement

---
+++ parallel_variable_assignment.rb
@@ -0,0 +1,29 @@
+# parallel variable assignment
+
+# a = 10
+# b = 20
+# c = 30
+
+a, b, c = 10, 20, 30
+
+p a
+p b
+p c
+
+print a, b, c
+puts a, b, c
+p a, b, c
+
+##############################
+
+a = 1
+b = 2
+
+p a, b
+
+# a = 2
+# b = 1
+
+a, b = b, a
+
+p a, b
