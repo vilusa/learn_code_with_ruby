3bef56fc16ccb8ac9be82cf673ca3bd677da99fc
Kemal AKIN <kemal.akin@lab2023.com>
2019-05-19 16:25:51 +0300

    Variables implement

---
+++ variables.rb
@@ -0,0 +1,24 @@
+name = "Kemal"
+last_name = "AKIN"
+handsome = true
+age = 20 + 1
+
+puts name
+puts last_name
+
+puts "Kemal" + " " + "AKIN"
+puts name + last_name
+
+puts handsome
+
+# puts "age" + 5 # no implicit conversion of Fixnum into String (TypeError)
+puts age + 5
+
+age = 50
+puts age
+
+favorite_number = 24
+p favorite_number
+
+favorite_number = "Twenty-four"
+p favorite_number
