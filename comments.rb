f77a1474d30ad87221c9a00d5abc5a3349ee60ee
Kemal AKIN <kemal.akin@lab2023.com>
2019-05-19 15:49:54 +0300

    Comments implement

---
+++ comments.rb
@@ -0,0 +1,15 @@
+# This is a comment
+
+# This prints out "Hello vilusa" to the Atom Runner output
+puts "Hello vilusa" # Prints "Hello vilusa" to the console
+
+# In this section of code,
+# we are going to do operations
+# A, B and C
+
+=begin
+puts 2 + 2
+puts 5 + 3
+puts "Hello World"
+p "Blah blah blah"
+=end
